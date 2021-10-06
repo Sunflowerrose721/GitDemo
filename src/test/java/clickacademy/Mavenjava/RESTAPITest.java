c9c5678e91beb35076368282db9dcf2098b1b22a
Sunflowerrose721 <sunflowerrose721@gmail.com>
2021-10-06 10:22:42 +0530

    this is commit from Asian Timezone guy

--- src/test/java/clickacademy/Mavenjava/RESTAPITest.java
+++ src/test/java/clickacademy/Mavenjava/RESTAPITest.java
@@ -7,6 +7,8 @@ public class RESTAPITest {
 	@Test
 	public void NativeAPpAndroid() {
 		System.out.println("NativeAPpAndroid");
+		System.out.println("NativeAPpAndroid");
+		System.out.println("NativeAPpAndroid");
 	}
 	@Test
 	public void IOSApps() {
